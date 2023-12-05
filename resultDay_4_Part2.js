d6f9e79f1c84e893264c4b2fea36566b4b871e97
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-12-05 11:44:01 +1100

    reduce runtime for part 2 to 1s

--- resultDay_4_Part2.js
+++ resultDay_4_Part2.js
@@ -1,19 +1,17 @@
 const {dataDay4}=require('./data');
 var sum=0;
 var numberOfDuplicate=[];
-var countOpti=0;
 for(let i=0; i<dataDay4.length;i++){
     numberOfDuplicate.push(1);
 }
 for(let i=0; i< numberOfDuplicate.length; i++){
     while(numberOfDuplicate[i]>0){
-        
-        numberOfDuplicate[i]--;
+
         let cardDetail=dataDay4[i].split(':')[1];
         let winningNumbers=cardDetail.split('|')[0].replaceAll(/\B\s/g,"").substring(0, cardDetail.split('|')[0].replace(/\B\s/g,"").length-1).split(' ');//array1
         let numbersOnCard=cardDetail.split('|')[1].replaceAll(/\B\s/g,"").split(' ');//array2
         let countWin=0;
-        if(countOpti===0){
+
         for(let j=0; j<winningNumbers.length; j++){
            //determine how many win in a card
             for(let k=0; k<numbersOnCard.length; k++){
@@ -22,20 +20,15 @@ for(let i=0; i< numberOfDuplicate.length; i++){
                     countWin++;
                 } 
             }
-           
-        }}
-        else countWin=countOpti;
-        
-        if(numberOfDuplicate[i]>0) countOpti=countWin;
-        else countOpti=0;
-
+        }
+        //add number of coppy card to the number of coppy card array
         if(countWin>0){
             for(let a=1; a<=countWin; a++){
-                if((i+a)<dataDay4.length) numberOfDuplicate[i+a]++;
+                if((i+a)<dataDay4.length) numberOfDuplicate[i+a]=numberOfDuplicate[i+a]+numberOfDuplicate[i];
             }
         }
-        
-        sum=sum+1;
+        sum=sum+numberOfDuplicate[i];
+        numberOfDuplicate[i]=0;
     }
 }
 console.log(sum);
